65ad554a2a8588d3ca14dd4e38bda1ff3514289b
Piyush7R <142214168+Piyush7R@users.noreply.github.com>
2024-11-19 16:22:21 +0530

    Structure.c

--- Structure.c
+++ Structure.c
@@ -4,10 +4,13 @@ struct Student{
     int roll;
     char name[20];
 };
+
 int main(){
     struct Student stud;
+    // Receiving the value
     scanf("%d", &stud.roll);
     scanf(" %[^\n]s", stud.name);
+    // Printing the value
     printf("The roll is : %d\n", stud.roll);
     printf("The name is : %s\n", stud.name);
     return 0;
